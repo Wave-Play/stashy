3cf3afc38f1bf8390884fec072a7bb210fc5d59e
Pkmmte Xeleon <hi@pkmmte.com>
2022-12-15 23:17:01 -0800

    ci: reverted back to simple syntax for "branches"
    I got the previous example out of the semantic-release docs. :/

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	release: {
-		branches: ['main', 'master', { name: 'beta', prerelease: true }]
+		branches: ['main', 'master', 'beta']
 	},
 	repositoryUrl: 'https://github.com/Wave-Play/stashy'
 };
