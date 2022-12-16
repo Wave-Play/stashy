e599c29a167a797a7f5f455a95ad8ed05139624d
Pkmmte Xeleon <hi@pkmmte.com>
2022-12-15 23:15:09 -0800

    ci: ensure only release.config.js is used
    No longer define "release" in the "package.json" for semantic-release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	release: {
-		branches: ['main', 'master', 'beta']
+		branches: ['main', 'master', { name: 'beta', prerelease: true }]
 	},
 	repositoryUrl: 'https://github.com/Wave-Play/stashy'
 };
